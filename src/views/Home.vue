4d2625991e8fe9b6e8b538b839361375ea932e92
hankivan <chen.ivan@livemail.tw>
2022-01-02 14:10:16 +0800

    first version to start a first coding project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hi, this is YiFan's tutorial project</h1>
+    <h2>I set a basic vue project for beginner. It's easy to test most functions.</h2>
+    <h3>There're three biggest front-end tools for now: AngularJS / ReactJS / VueJS</h3>
+    <p>VueJS is the easiest one so it's pretty good to start your first project</p>
+    <a href="https://vuejs.org">Go check Vue Official Document</a>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+<style scoped>
+.home {
+  text-align:center;
+  margin-top:50px;
+}
+</style>
